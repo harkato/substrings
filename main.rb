c7903ec63697555694bd8dbbea66dcddb449cc66
harkato <92404938+harkato@users.noreply.github.com>
2023-08-27 22:12:56 -0300

    Finish substrings algorithm

--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+string = "Howdy partner, sit down! How's it going?"
+
 def substrings(string, dictionary)
-  dictionary.reduce({}) do |key, value|
-    if key[value] == string
-      key
+    new_str = string.downcase.split(' ')
+    matches = dictionary.map do |element|
+        new_str.to_s.scan(element)
     end
-  end
+    collection = matches.flatten.inject(Hash.new(0)) do |total, e|
+        total[e] += 1
+        total
+    end
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-substrings("go", dictionary)
+puts substrings(string, dictionary)
